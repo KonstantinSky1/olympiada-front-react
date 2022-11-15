96d340a4713445276fbe416ff0c934e1b92bc1f9
konstantin <kot_sky@mail.ru>
2022-11-15 18:16:06 +0600

    pagination news

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,4 +1,6 @@
-import React, { useContext  } from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { Pagination } from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 import './News.css';
 
@@ -9,12 +11,35 @@ function News() {
   // Тестируем получение данных с API
   const { posts } = useContext(JsonPlaceholderContext);
 
+  const [page, setPage] = useState(1);
+  const [pageQuantity, setPageQuantity] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+  const [currentItems, setCurrentItems] = useState([]);
+
+  const itemsPerPage = 5;
+
+  // Для изменения размера Pagination от разрешения экрана:
+  const matches = useMediaQuery('(max-width:411px)');
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+
+    setCurrentItems(posts.slice(itemOffset, endOffset));
+    setPageQuantity(Math.ceil(posts.length / itemsPerPage));
+  }, [posts, itemsPerPage, itemOffset]);
+
+  function handleChangePage(num) {
+    const newOffset = ((num-1) * itemsPerPage) % posts.length;
+
+    setItemOffset(newOffset);
+  }
+
   return (
     <section className="all-news">
       <h2 className="all-news__title">Все новости</h2>
       <ul className="all-news__list">
         {
-          posts && posts.map(post => {
+          currentItems && currentItems.map(post => {
             return <NewsItem
                       key={post.id}
                       post={post}
@@ -22,6 +47,23 @@ function News() {
           })
         }
       </ul>
+      {
+        !!pageQuantity && (
+          <Pagination
+            count={pageQuantity}
+            page={page}
+            sx={{
+              marginX: 'auto',
+              width: 'fit-content'
+            }}
+            onChange={(_, num) => {
+              handleChangePage(num)
+              setPage(num)}
+            }
+            size={`${matches && "small"}`}
+          />
+        )
+      }
     </section>
   );
 }
